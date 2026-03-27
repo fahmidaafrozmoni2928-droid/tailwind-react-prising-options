f6d6ba6e897cc714824cc22aff9ee9a78b9af361
fahmidaafrozmoni2928-droid <fahmidaafrozmoni2928@gmail.com>
2026-03-27 23:01:42 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import DaisyNav from './components/DaisyNav/DaisyNav'
+import NavBar from './NavBar/NavBar'
+
+function App() {
+  
+
+  return (
+    <>
+
+
+      
+        <header>
+          <NavBar></NavBar>
+          <DaisyNav></DaisyNav>
+        </header>
+         
+         </>
+  )
+}
+
+export default App
---
+++ src/NavBar/NavBar.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const navLinks = [
+    {
+      id: 1,
+      name: "Home",
+      path: "/"
+    },
+    {
+      id: 2,
+      name: "About",
+      path: "/about"
+    },
+    {
+      id: 3,
+      name: "Services",
+      path: "/services"
+    },
+    {
+      id: 4,
+      name: "Blog",
+      path: "/blog"
+    },
+    {
+      id: 5,
+      name: "Contact",
+      path: "/contact"
+    }
+  ];
+
+const NavBar = () => {
+    return (
+        <nav >
+
+        <ul className="flex">
+            {
+                navLinks.map(route =>  <li className="mr-10"><a href={route.path}></a>{route.name}</li>)
+            }
+        </ul>
+
+
+         {/* < ul className="flex">
+            <li className="mr-10"><a href="/home"></a>Home</li>
+            <li className="mr-10"><a href="/about"></a>About</li>
+            <li className="mr-10"><a href="/blog"></a>Blog</li>
+          </ul >*/}
+        </nav>
+    )
+}
+export default NavBar;
